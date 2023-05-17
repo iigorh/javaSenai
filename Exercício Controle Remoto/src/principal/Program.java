d4881e14c7fa3108d1eda489bf82f98ad3a85acf
Igor <igorhenriquepps@gmail.com>
2023-05-17 14:54:45 -0300

    Exercício de controle remoto

--- Exercício Controle Remoto/src/principal/Program.java
+++ Exercício Controle Remoto/src/principal/Program.java
@@ -1,10 +1,8 @@
 package principal;
 
-<<<<<<< HEAD
+
 import java.util.Scanner;
 
-=======
->>>>>>> 1561a0330476f70cd9ae7ca6536263a748ee424e
 import entities.Televisor;
 
 public class Program {
@@ -12,15 +10,12 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Televisor tv = new Televisor();
-<<<<<<< HEAD
+
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("A televisão está desligada. Deseja ligá-la?");
 		System.out.println("Digite '1' para sim, e '2' para não.");
-=======
->>>>>>> 1561a0330476f70cd9ae7ca6536263a748ee424e
-		
-		
+		tv.ligarTv(sc.nextLine());
 		
 
 	}
